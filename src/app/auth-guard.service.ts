4d56bfcbd7d79e5395aff4a959522b25cc635176
Rafał <rafalhawelka@gmail.com>
2020-01-07 23:03:41 +0100

    before fix

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Router, CanActivate} from '@angular/router';
+import {AuthenticationService} from './authentication.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+    constructor(private _auth : AuthenticationService, private _router : Router) {}
+
+    canActivate() {
+        if (!this._auth.isLoggedIn()) {
+            this
+                ._router
+                .navigateByUrl('/');
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService, TokenPayload} from '../authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  credentials: TokenPayload = {
+    id: 0,
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: ''
+  }
+
+  constructor(private _auth: AuthenticationService, private router: Router) { }
+
+  login() {
+    this._auth.login(this.credentials).subscribe( () => {
+      this.router.navigateByUrl('/profile')
+    },
+    err => {
+      console.error(err);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
