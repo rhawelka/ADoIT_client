051e4120ac8f6dbd281a25c42ec0b09e528102ff
Rafał <rafalhawelka@gmail.com>
2020-01-12 20:19:17 +0100

    add v1.1 with some test

--- src/app/login-form/login-form.component.spec.ts
+++ src/app/login-form/login-form.component.spec.ts
@@ -1,31 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginFormComponent } from './login-form.component';
-import { FormsModule } from '@angular/forms';
-import { AuthenticationService } from '../services/authentication.service';
-import { HttpClient,HttpHandler } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
+import {LoginFormComponent} from './login-form.component';
+import {FormsModule} from '@angular/forms';
+import {AuthenticationService} from '../services/authentication.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginFormComponent', () => {
-  let component: LoginFormComponent;
-  let fixture: ComponentFixture<LoginFormComponent>;
+    let component : LoginFormComponent;
+    let fixture : ComponentFixture < LoginFormComponent >;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ FormsModule,RouterTestingModule.withRoutes([]) ],
-      declarations: [ LoginFormComponent ],
-      providers:[AuthenticationService, HttpClient,HttpHandler]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule, RouterTestingModule.withRoutes([])
+            ],
+            declarations: [LoginFormComponent],
+            providers: [AuthenticationService, HttpClient, HttpHandler]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture
+            .whenStable()
+            .then(() => {
+                component
+                    .loginForm
+                    .controls['first_name']
+                    .setValue('test');
+            })
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    it('initialises the form', () => {
+        expect(component).toBeTruthy();
+        expect(fixture).toBeTruthy();
+        expect(component)
+            .not
+            .toBeNull();
+        expect(fixture)
+            .not
+            .toBeNull();
+    });
 });
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,36 +1,49 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { RegisterComponent } from './register.component';
-import { FormsModule } from '@angular/forms';
-import { AuthenticationService } from '../services/authentication.service';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
+import {RegisterComponent} from './register.component';
+import {FormsModule} from '@angular/forms';
+import {AuthenticationService} from '../services/authentication.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+    let component : RegisterComponent;
+    let fixture : ComponentFixture < RegisterComponent >;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, FormsModule,HttpClientModule,RouterTestingModule.withRoutes([]) ],
-      declarations: [ RegisterComponent ],
-      providers: [AuthenticationService]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule, FormsModule, HttpClientModule, RouterTestingModule.withRoutes([])
+            ],
+            declarations: [RegisterComponent],
+            providers: [AuthenticationService]
+        }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have <label> with first name works>', () => {
+        const personElement : HTMLElement = fixture.nativeElement;
+        const label = personElement.querySelectorAll('label');
+        expect(label[0].textContent).toEqual('First Name');
+    });
+    it('initialises the form', () => {
+      expect(component).toBeTruthy();
+      expect(fixture).toBeTruthy();
+      expect(component).not.toBeNull();
+      expect(fixture).not.toBeNull();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-  afterEach(() => {
-    TestBed.resetTestingModule();
 });
+afterEach(() => {
+    TestBed.resetTestingModule();
 });
